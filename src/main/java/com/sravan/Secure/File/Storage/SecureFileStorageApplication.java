18ece70041eb57732614edc5501dd65f25973f82
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-03-22 16:01:15 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/sravan/Secure/File/Storage/SecureFileStorageApplication.java
@@ -0,0 +1,14 @@
+package com.sravan.Secure.File.Storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileStorageApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SecureFileStorageApplication.class, args);
+	}
+
+}
