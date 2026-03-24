c23b99df2edead9b897cc05cf3ad6541523d6b39
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-03-24 16:24:33 +0530

    Done with JWT Token generation for authentication

---
+++ src/main/java/com/sravan/Secure/File/Storage/Security/JwtService.java
@@ -0,0 +1,22 @@
+package com.sravan.Secure.File.Storage.Security;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+    private final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    public String GenerateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()*1000*60*60))
+                .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
+                .compact();
+    }
+}
