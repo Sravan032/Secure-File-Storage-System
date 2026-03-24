66cec4d6322f9d33ccbea8bc347c76a89630aadc
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-03-24 16:25:45 +0530

    Finally fixed bugs and improve authentication flow

---
+++ src/main/java/com/sravan/Secure/File/Storage/Controller/Controller.java
@@ -0,0 +1,12 @@
+package com.sravan.Secure.File.Storage.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/home")
+    public String hello(){
+        return "Hi User";
+    }
+}
---
+++ src/test/java/com/sravan/Secure/File/Storage/SecureFileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravan.Secure.File.Storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
