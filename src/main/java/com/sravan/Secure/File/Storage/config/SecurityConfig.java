bd032c58813de6722af3f218c82738ad029a8f5c
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-03-28 14:48:23 +0530

    Configured Security for allowing login/register requests and added JWT filter

--- src/main/java/com/sravan/Secure/File/Storage/config/SecurityConfig.java
+++ src/main/java/com/sravan/Secure/File/Storage/config/SecurityConfig.java
@@ -1,14 +1,39 @@
 package com.sravan.Secure.File.Storage.config;
 
+import com.sravan.Secure.File.Storage.Security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
+                    .anyRequest().authenticated()
+            )
+
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
