92d6ac9567766a32844d7e0fb5a4003b86e5f903
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-07-21 09:44:50 +0530

    Simplify FileController using Spring Security authentication

--- src/main/java/com/sravan/Secure/File/Storage/Controller/FileController.java
+++ src/main/java/com/sravan/Secure/File/Storage/Controller/FileController.java
@@ -2,28 +2,26 @@ package com.sravan.Secure.File.Storage.Controller;
 
 import com.sravan.Secure.File.Storage.Service.FileService;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @RestController
-@RequestMapping ("/api/files")
+@RequestMapping("/api/files")
 public class FileController {
+
     private final FileService fileService;
-    public FileController(FileService fileService){
-        this.fileService=fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
     }
 
     @PostMapping("/upload")
-    public String UploadFile(@RequestParam("file")MultipartFile file, @RequestHeader("Authorization") String AuthHeader) throws IOException {
-        String token = AuthHeader.substring(7);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("TOKEN: " + token);
-        String username = auth.getName();
+    public String uploadFile(
+            @RequestParam("file") MultipartFile file,
+            Authentication authentication) throws IOException {
 
-        return fileService.Uploadfile(file,username);
+        return fileService.uploadFile(file, authentication.getName());
     }
 }
