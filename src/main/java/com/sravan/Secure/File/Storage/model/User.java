17691e74500a7180ae82ba96498c92de7b81aa84
Sravan032 <167642797+Sravan032@users.noreply.github.com>
2026-07-27 20:39:42 +0530

    Refactor User entity with proper encapsulation and JPA mappings

--- src/main/java/com/sravan/Secure/File/Storage/model/User.java
+++ src/main/java/com/sravan/Secure/File/Storage/model/User.java
@@ -1,57 +1,80 @@
 package com.sravan.Secure.File.Storage.model;
 
+import com.sravan.Secure.File.Storage.model.FileEntity;
+
+import java.util.List;
+
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
-    String username;
+    private String username;
 
     @Column(nullable = false)
-    String password;
+    private String password;
 
     @Column(nullable = false)
-    String role;
+    private String role;
 
-    public User(){}
+    @OneToMany(mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true)
+    private List<FileEntity> files = new ArrayList<>();
 
-    public User(String username, String password, String role){
-        this.username =username;
-        this.password=password;
-        this.role=role;
+    public User() {}
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
-    public long getId(long id){
+    public Long getId() {
         return id;
     }
-    public void setId(long id) {
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getName(String username){
+    public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
-    public void setPassword(String password){
-        this.password=password;
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getRole(String role){
+    public String getRole() {
         return role;
     }
-    public void setRole(String role){
-        this.role=role;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<FileEntity> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<FileEntity> files) {
+        this.files = files;
     }
 }
